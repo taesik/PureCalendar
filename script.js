2905818db8afcc63321a074e3b4b75890999fbf8
taesik <64353197+taesik@users.noreply.github.com>
2022-04-19 19:03:31 +0900

    add date when clicked on innertext

--- script.js
+++ script.js
@@ -17,15 +17,18 @@ function openModal(date) {
   clicked = date;
 
   const eventForDay = events.find(e => e.date === clicked);
+  const clickedDate = document.getElementById('clicked_date');
+  clickedDate.innerText = date;
 
   if (eventForDay) {
     document.getElementById('eventText').innerText = eventForDay.title;
     deleteEventModal.style.display = 'block';
   } else {
+
     newEventModal.style.display = 'block';
   }
 
-  backDrop.style.display = 'none';
+  // backDrop.style.display = 'none';
 }
 
 function load() {
