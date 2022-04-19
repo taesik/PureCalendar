5fb241f96e75055083dc52503b2d2e9d065ea3ca
taesik <64353197+taesik@users.noreply.github.com>
2022-04-19 11:41:37 +0900

    change px

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let nav = 0;
 let clicked = null;
-let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
+let events = localStorage.getItem('events')
+   ?
+   JSON.parse(localStorage.getItem('events'))
+   :
+   [];
 
 const calendar = document.getElementById('calendar');
 const newEventModal = document.getElementById('newEventModal');
